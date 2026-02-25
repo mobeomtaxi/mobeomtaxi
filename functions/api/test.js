c24c5df8b857472ffdcdc5577b757ce903e23026
mobeomtaxi <honestkid03@gmail.com>
2026-02-26 00:53:03 +0900

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,3 +1,9 @@
-export async function onRequest() {
-  return new Response("OK - Functions works");
+export async function onRequest({ env }) {
+  const result = await env.DB
+    .prepare("SELECT name FROM sqlite_master WHERE type='table'")
+    .all();
+
+  return new Response(JSON.stringify(result), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
